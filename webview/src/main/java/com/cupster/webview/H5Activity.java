0b6845a2e2c4da7771689ce52eba97c28f809e0c
CupsterHub <36061791+CupsterHub@users.noreply.github.com>
2020-03-21 01:02:28 +0800

    easy modify

--- webview/src/main/java/com/cupster/webview/H5Activity.java
+++ webview/src/main/java/com/cupster/webview/H5Activity.java
@@ -33,7 +33,7 @@ import static android.view.KeyEvent.KEYCODE_BACK;
 public class H5Activity extends AppCompatActivity {
 
     private static final String TAG = "H5Activity";
-    private static final String APP_CACAHE_DIRNAME = "/didigoweb";
+    private static final String APP_CACAHE_DIRNAME = "/didiweb";
     public static final String KEY_H5_URL = "H5_URL";
 
     TextView mToolbarTitle;
@@ -244,39 +244,34 @@ public class H5Activity extends AppCompatActivity {
             });
         }
 
-        //setting 设置
-        //声明WebSettings子类
+        //设置
         WebSettings webSettings = mWebView.getSettings();
-        //如果访问的页面中要与Javascript交互，则webview必须设置支持Javascript
-        // 若加载的 html 里有JS 在执行动画等操作，会造成资源浪费（CPU、电量）
-        // 在 onStop 和 onResume 里分别把 setJavaScriptEnabled() 给设置成 false 和 true 即可
+        //设置支持Javascript
         webSettings.setJavaScriptEnabled(true);
-
-        //设置自适应屏幕，两者合用
+        //设置自适应
         webSettings.setUseWideViewPort(true); //将图片调整到适合webview的大小
         webSettings.setLoadWithOverviewMode(true); // 缩放至屏幕的大小
-        //缩放操作
-        webSettings.setSupportZoom(true); //支持缩放，默认为true。是下面那个的前提。
-        webSettings.setBuiltInZoomControls(true); //设置内置的缩放控件。若为false，则该WebView不可缩放
+        //缩放
+        webSettings.setSupportZoom(true); //支持缩放，默认为true。是setBuiltInZoomControls(true)的前提。
+        webSettings.setBuiltInZoomControls(true); //设置内置的缩放控件。若为false=不可缩放
         webSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
         //其他细节操作
-        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); //关闭webview中缓存
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
         //缓存模式如下：
         //LOAD_CACHE_ONLY: 不使用网络，只读取本地缓存数据
         //LOAD_DEFAULT: （默认）根据cache-control决定是否从网络上取数据。
         //LOAD_NO_CACHE: 不使用缓存，只从网络获取数据.
         //LOAD_CACHE_ELSE_NETWORK，只要本地有，无论是否过期，或者no-cache，都使用缓存中的数据。
-        webSettings.setAllowFileAccess(true); //设置可以访问文件
-
-        webSettings.setDomStorageEnabled(true); // 开启 DOM storage API 功能
-        webSettings.setDatabaseEnabled(true);   //开启 database storage API 功能
-        webSettings.setAppCacheEnabled(true);//开启 Application Caches 功能
+        webSettings.setAllowFileAccess(true);//可以访问文件
+        webSettings.setDomStorageEnabled(true);// 开启DOM storage
+        webSettings.setDatabaseEnabled(true);//开启database storage
+        webSettings.setAppCacheEnabled(true);//开启Application Caches
 
         String cacheDirPath = getFilesDir().getAbsolutePath() + APP_CACAHE_DIRNAME;
-        webSettings.setAppCachePath(cacheDirPath); //设置  Application Caches 缓存目录
+        webSettings.setAppCachePath(cacheDirPath);//设置Caches缓存目录
         //每个 Application 只调用一次 WebSettings.setAppCachePath()，WebSettings.setAppCacheMaxSize()
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(true); //支持通过JS打开新窗口
-        webSettings.setLoadsImagesAutomatically(true); //支持自动加载图片
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true); //通过JS打开新窗口
+        webSettings.setLoadsImagesAutomatically(true); //自动加载图片
         webSettings.setDefaultTextEncodingName("utf-8");//设置编码格式
     }
 
